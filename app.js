9b7dd99b17353b9fa304c76dc0422aea0f8cfbe2
tsumitakaoru <k.tsumita01@gmail.com>
2022-09-03 16:05:11 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import bodyParser from "body-parser";
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+mongoose.connect(`mongodb://localhost:27017/userDB`);
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const User = mongoose.model("user", userSchema);
+
+app.route('/')
+.get((req, res) => {
+  res.render("home");
+});
+
+app.route('/login')
+  .get((req, res) => {
+    res.render("login");
+  });
+
+app.route('/register')
+  .get((req, res) => {
+    res.render("register");
+  })
+
+  .post(async (req, res) => {
+    const newUser = new User ({
+      email: req.body.username,
+      password: req.body.password
+    })
+    try {
+      newUser.save();
+      res.render("secrets");
+    }
+    catch (err) {
+      res.send(err);
+    }
+  })
+
+
+
+
+let port = process.env.PORT;
+if (port == null || port == '') {
+  port = 3001;
+}
+
+app.listen(port, (req, res) => {
+  console.log(`I'm listening ${port}`);
+})
